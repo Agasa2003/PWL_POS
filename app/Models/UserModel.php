8576b3a790b671ee993f29b8ef8287e9585eb626
Ananda Galang Saputra <saputraananda343@gmail.com>
2024-05-28 11:47:01 +0700

    Praktikum 1  pertemuan 11 – Implementasi Eloquent Accessor

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,36 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-
-class UserModel extends Authenticatable implements JWTSubject
-{
-    
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-    
+class UserModel extends Authenticatable implements JWTSubject {
+    use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-
     protected $fillable = [
         'level_id',
         'username',
         'nama',
         'password',
+        'image'
     ];
 
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims() {
+        return [];
+    }
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function image(): Attribute {
+        return Attribute::make(
+            get: fn ($image) => url('storage/posts/' . $image),
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
@@ -18,8 +19,15 @@ use App\Http\Controllers\Api\LogoutController;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+Route::get('levels', [LevelController::class, 'index']);
+    Route::post('levels', [LevelController::class, 'store']);
+    Route::get('levels/{level}', [LevelController::class, 'show']);
+    Route::put('levels/{level}', [LevelController::class, 'update']);
+    Route::delete('levels/{level}', [LevelController::class, 'destroy']);
